648b057fda4a0e74ce7ff527748560270bf4d0f3
Marcus Widen <hellowiden@gmail.com>
2024-10-29 16:13:47 +0100

    Initial commit with Navbar component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,19 @@
 // MARCUS
 // src/components/Navbar/Navbar.jsx
 
+
+// Navbar.jsx
 import React from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <a href="/">Home</a>
-      </div>
-      <ul className="navbar-links">
-        <li>
-          <a href="/login">Login</a>
-        </li>
-        <li>
-          <a href="/events">Events</a>
-        </li>
-        <li>
-          <a href="/profile">Profile</a>
-        </li>
+      <a href="/" className="logo">Logo</a>
+      <ul className="nav-links">
+        <li><a href="#events">Events</a></li>
+        <li><a href="#profile">Profile</a></li>
+        <li><a href="#login">Login</a></li>
       </ul>
     </nav>
   );
